aabe3ac6fdf3d4e964c97dabc41d23cced83dff5
Vishnuprasanth-mediwave <vishnu_prasanth@mindwaveventures.com>
2023-10-16 19:07:21 +0530

    first commit

---
+++ app/src/components/editForm.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function EditForm({ todos }) {
+  //   const [editValue, setEditValue] = useState("");
+  return (
+    <form>
+      <div>
+        <input type="text" name="" id="" value="hello" />
+
+        <button>Update</button>
+      </div>
+    </form>
+  );
+}
+export default EditForm;
